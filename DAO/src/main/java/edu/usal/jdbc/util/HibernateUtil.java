9a371ea4c81d3d33e446600b1ad6268a0e276027
luciopalmucci <lucio.palmucci@hotmail.com>
2026-08-14 16:19:19 -0300

    Set up docker

--- DAO/src/main/java/edu/usal/jdbc/util/HibernateUtil.java
+++ DAO/src/main/java/edu/usal/jdbc/util/HibernateUtil.java
@@ -8,13 +8,25 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 public class HibernateUtil {
 
     public static SessionFactory getSessionFactory(){
-        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
-                .configure()
-                .build();
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                .configure();
+
+        aplicarSiExiste(builder, "hibernate.connection.url", "DB_URL");
+        aplicarSiExiste(builder, "hibernate.connection.username", "DB_USER");
+        aplicarSiExiste(builder, "hibernate.connection.password", "DB_PASSWORD");
+
+        StandardServiceRegistry registry = builder.build();
 
         return new MetadataSources(registry)
                 .buildMetadata()
                 .buildSessionFactory();
+    }
+
+    private static void aplicarSiExiste(StandardServiceRegistryBuilder builder, String propiedad, String variableDeEntorno) {
+        String valor = System.getenv(variableDeEntorno);
+        if (valor != null && !valor.isBlank()) {
+            builder.applySetting(propiedad, valor);
+        }
     }
 
 }
---
+++ Dockerfile
@@ -0,0 +1,14 @@
+LABEL authors="Lucio"
+
+# Etapa 1: compila DAO y MVC con Maven
+FROM maven:3.9-eclipse-temurin-11 AS build
+WORKDIR /app
+COPY . .
+RUN mvn clean package -DskipTests
+
+# Etapa 2: corre el WAR resultante en Tomcat
+FROM tomcat:9.0-jdk11-temurin
+RUN rm -rf /usr/local/tomcat/webapps/ROOT
+COPY --from=build /app/MVC/target/*.war /usr/local/tomcat/webapps/ROOT.war
+EXPOSE 8080
+CMD ["catalina.sh", "run"]
